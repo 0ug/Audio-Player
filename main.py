011e1cfd4fec2b409646d36fd8d7618014132376
StringIO <73809660+StringIO@users.noreply.github.com>
2021-12-18 11:47:51 +0900

    Update main.py

--- main.py
+++ main.py
@@ -4,16 +4,22 @@ import tkinter.messagebox as messagebox
 import tkinter.filedialog as filedialog
 
 try:
-    import subprocess, os, requests, zipfile, shutil, io, pathlib, pythread, atexit, sys
+    import subprocess, os, requests, zipfile, shutil, io, pathlib, pythread, atexit, sys, time, warnings
 except:
     exit(2) 
 
+if not sys.platform.startswith("win"):
+    warnings.warn("Windows에서만 작동합니다.", RuntimeWarning, stacklevel=2)
+    time.sleep(10)
+    exit(3)
+
 def select_file():
-    global _song_q, _song_var, _song_q_n
-    file = filedialog.askopenfilename()
+    global _song_q, _song_q_n
+    file = filedialog.askopenfilename(title="파일 선택하기")
 
     if file in _song_q: return messagebox.showwarning("경고", "이미 선택된 음악입니다.")
     if file == "": return None
+    if not pathlib.Path(file).suffix in (".mp3", ".ogg", ".wav"): return messagebox.showwarning("경고", "음악 파일이 아닙니다.")
 
     _song_q.append(file)
 
@@ -26,9 +32,7 @@ def select_file():
         _song_q_n.append(name)
         print(f"{name}: {song}")
 
-    _song_str = "\n".join(_song_q_n)
-
-    return _song_var.set(f"Song List\n{_song_str}")
+    return None
 
 def install_ffplay():
     if not os.path.isdir("tmp"):
@@ -47,19 +51,26 @@ def install_ffplay():
     return None
 
 def play():
-    global _ffplay, _song_q
+    global _ffplay, _song_q, _enable_song
     for queue in _song_q:
-        subprocess.call([_ffplay, queue, "-autoexit", "-nodisp"])
-
+        subprocess.call([_ffplay, queue, "-nodisp", "-autoexit"])
 def on_exit():
     global root
     root.withdraw()
-    sys.exit(0)
+    exit()
 
 def on_exit_():
     global root
     os.system("taskkill /f /im ffplay.exe")    
     root.destroy()
+
+def song_list_():
+    global _song_q_n
+    _root = Tk()
+    _root.resizable(False, False)
+    _label = Label(_root, text="\n".join(_song_q_n))
+    _label.pack()
+    _root.mainloop()
 
 atexit.register(on_exit_)
 
@@ -70,19 +81,19 @@ root = Tk()
 root.geometry("265x265")
 root.resizable(False, False)
 root.title("Plpyer")
-
-_song_var = StringVar(value="Song List")
+ 
 _ffplay = f"{os.getcwd()}\\ext\\ffplay.exe"
 _song_q = list() # _song_queue = []
 _song_q_n = list() # _song_q_n = []
+_enable_song = False
 
 select_btn = Button(root, text="음악 선택하기", command=select_file)
 play_btn = Button(root, text="음악 재생하기", command=lambda: pythread.cTread("play", play))
-q_lab = Label(root, textvariable=_song_var)
+list_btn = Button(root, text="음악 목록 보기", command=song_list_)
 
 root.protocol("WM_DELETE_WINDOW", on_exit)
 
 select_btn.pack()
 play_btn.pack()
-q_lab.pack()
+list_btn.pack()
 root.mainloop()
