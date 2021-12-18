3b6c0ad985d195c5b8384beb052c9517460c4055
Poolmanager <73809660+poolmanager@users.noreply.github.com>
2021-12-18 22:04:43 +0900

    Update main.py

--- main.py
+++ main.py
@@ -3,11 +3,12 @@ from tkinter import *
 import tkinter.messagebox as messagebox
 import tkinter.filedialog as filedialog
 
+import sys
+import pip
+import os
+
 try:
-    import os
-    import sys
     import ctypes
-    import warnings
     import time
     import pathlib
     import requests
@@ -17,15 +18,38 @@ try:
     import shutil
     import subprocess
     import atexit
-except:
-    sys.exit(2)
+    import js2py
+    import PIL.Image
+except ImportError:
+    if hasattr(pip, "main"):
+        pip.main(["install", "js2py requests pythread pillow"])
+    else:
+        os.system("pip install js2py requests pythread pillow")
+
+log = js2py.eval_js(
+"""
+function log(log){
+    console.log(log);
+}
+"""
+)
+
+warning = js2py.eval_js(
+"""
+function warning(log){
+    console.log('warning: ' + log);
+}
+
+"""
+)
 
 ctypes.windll.shcore.SetProcessDpiAwareness(1)
 
-print("\033[32m" + "Plpyer | 제작: poolmanager | https://github.com/poolmanager")
+print("\033[32m")  
+log("Plpyer | 제작: poolmanager | https://github.com/poolmanager")
 
 if not sys.platform.startswith("win"):
-    warnings.warn("Windows에서만 작동합니다.", RuntimeWarning, stacklevel=2)
+    warning("윈도우에서만 작동합니다.")
     time.sleep(10)
     sys.exit(3)
 
@@ -67,20 +91,29 @@ def install_ffplay():
     return None
 
 def play():
-    global _ffplay, _song_q, _enable_song
-    for queue in _song_q:
-        subprocess.call([_ffplay, queue, "-nodisp", "-autoexit"])
-        
+    global _ffplay, _song_q, log, _T_var
+    def wrap():
+        for queue in _song_q:
+            music = "Now playing: {0}".format(os.path.basename(str(queue).replace("\\", os.sep).replace(pathlib.Path(queue).suffix, "")))
+            log(music)
+            _T_var.set(music)
+            subprocess.call([_ffplay, queue, "-nodisp", "-autoexit"], stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT)
+
+    return pythread.cTread("PlayWrapper", wrap)
+
 def on_exit():
     global root
+    subprocess.call("taskkill /f /im ffplay.exe")
     root.withdraw()
     sys.exit(0)
 
 def on_exit_():
     global root
-    os.system("taskkill /f /im ffplay.exe")
+    subprocess.call("taskkill /f /im ffplay.exe")
     print("\033[37m")  
     root.destroy()
+
+atexit.register(on_exit_)
 
 def song_list_():
     global _song_q_n
@@ -93,9 +126,11 @@ def song_list_():
     _label.pack()
     _root.mainloop()
 
-atexit.register(on_exit_)
+def stop():
+    subprocess.call("taskkill /f /im ffplay.exe")    
 
 if not os.path.isdir("ext") & os.path.isfile("ext/ffplay.exe"):
+    warning("FFplay를 설치 중입니다. 약간의 시간이 걸릴 수 있습니다.")
     install_ffplay()
 
 root = Tk()
@@ -106,24 +141,31 @@ root_bg = "images/background.png" if int(root_dpi) < 110 else "images/background
 root.geometry(root_res)
 root.resizable(False, False)
 root.title("Plpyer")
+_T_var = StringVar(value="재생중: 공기 소리")
  
 _ffplay = f"{os.getcwd()}\\ext\\ffplay.exe"
 _song_q = list() # _song_queue = []
 _song_q_n = list() # _song_q_n = []
-_enable_song = False
-_canvas = Canvas(root, bg="blue", height=root_res_bg, width=root_res_bg)
+canvas = Canvas(root, bg="blue", height=root_res_bg, width=root_res_bg)
 _img = PhotoImage(file=root_bg)
 _bg = Label(root, image=_img)
 _bg.place(x=0, y=0, relwidth=1, relheight=1)
+_rgb_t = PIL.Image.open("images/background.png")
+_rgb_img = _rgb_t.convert("RGB")
+r, g, b = _rgb_img.getpixel((1, 1))
 
-select_btn = Button(root, text="음악 선택하기", command=select_file, bg="white", fg="black")
-play_btn = Button(root, text="음악 재생하기", command=lambda: pythread.cTread("play", play), bg="white", fg="black")
-list_btn = Button(root, text="음악 목록 보기", command=song_list_, bg="white", fg="black")
+select_btn = Button(root, text="음악 선택", command=select_file, bg="white", fg="black")
+play_btn = Button(root, text="음악 재생", command=play, bg="white", fg="black")
+stop_btn = Button(root, text="음악 중지", command=stop, bg="white", fg="black")
+list_btn = Button(root, text="목록 보기", command=song_list_, bg="white", fg="black")
+song_lab = Label(root, textvariable=_T_var, bg=f"#{r:02x}{g:02x}{b:02x}", fg="white")
 
 root.protocol("WM_DELETE_WINDOW", on_exit)
 
 select_btn.pack()
 play_btn.pack()
+stop_btn.pack()
 list_btn.pack()
-_canvas.pack()
+song_lab.pack()
+canvas.pack()
 root.mainloop()
